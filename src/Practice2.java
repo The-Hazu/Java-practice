7cbcfc3a2a1eb9c077e0f7a920561c7f4813856e
Бекжан <bekzhankasymovx@gmail.com>
2026-08-04 01:51:12 +0500

    Second practice:Guess the number

--- src/Practice2.java
+++ src/Practice2.java
@@ -1,6 +1,5 @@
 import java.util.Random;
 import java.util.Scanner;
-
 public class Practice2 {
     public static void main(String[] args) {
         int targetNumber = new Random().nextInt(100)+1;
